cd0b4653bb91ac2c5e34f7db5dab0347bf90bf91
MonolithOfficial <cotneaburjania@gmail.com>
2023-07-18 19:58:04 +0400

    user 2 did something on master

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Changes on branch1");
+        System.out.println("User 2 did something");
     }
 }
