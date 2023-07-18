0425257e3f9e8f3d86aea6e00768d66e459a584e
MonolithOfficial <cotneaburjania@gmail.com>
2023-07-18 19:56:28 +0400

    user 1 did something

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Changes on branch1");
+        System.out.println("Changes AGAIN by user1");
     }
 }
