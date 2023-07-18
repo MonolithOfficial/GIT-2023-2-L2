93f9684bed338ea86f03ba478f5648f7838654f3
MonolithOfficial <cotneaburjania@gmail.com>
2023-07-18 19:53:25 +0400

    changed print function

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Changes on master");
+        System.out.println("Changes on branch1");
     }
 }
