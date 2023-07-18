379a1f51d9c35100b8281ca6d656cf236c6c9d18
MonolithOfficial <cotneaburjania@gmail.com>
2023-07-18 20:26:57 +0400

    added random (user1)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,12 @@
 package org.example;
 
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Changes on branch1");
         System.out.println("User 2 did something");
         System.out.println("Changes AGAIN by user1");
+        Random random = new Random();
     }
 }
